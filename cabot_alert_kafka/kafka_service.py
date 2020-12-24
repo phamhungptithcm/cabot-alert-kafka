a9974e9830fb5a243c51c968f87590fe097c2c88
Phạm Hùng <32831453+phamhungptithcm@users.noreply.github.com>
2020-12-24 01:48:03 -0600

    Update kafka_service.py

--- cabot_alert_kafka/kafka_service.py
+++ cabot_alert_kafka/kafka_service.py
@@ -2,17 +2,22 @@ from kafka import KafkaAdminClient, KafkaProducer
 from kafka.admin import NewTopic
 from kafka.errors import KafkaError
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class KafkaService():
+    name='KafkaService'
     def produceMessage(self, bootstrapServer, topic, jsonMessage):
         try:
             producer = KafkaProducer(bootstrap_servers=bootstrapServer)
-            print('Sending message to topic %s ' % topic)
+            logger.info('Sending message to topic %s ' % topic)
             producer.send(topic, jsonMessage)
-            print('Sent successfully')
+            logger.info('Sent successfully')
             producer.close()
         except KafkaError as e:
-            print('Error to send kafka %s' % e)
+            logger.error('Error to send kafka %s' % e)
             pass
 
     def createTopic(self,bootstrapServer,topic, numPartitions, replicationFactor):
@@ -23,7 +28,7 @@ class KafkaService():
                              replication_factor=replicationFactor)
             admin.create_topics([topic])
         except Exception as e:
-            print('Error to create topic %s' % e)
+            logger.error('Error to create topic %s' % e)
             pass
 
 
