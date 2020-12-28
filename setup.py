c62c1450a276e1c4f47f4675f54c22ea428f4050
Phạm Hùng <32831453+phamhungptithcm@users.noreply.github.com>
2020-12-28 03:09:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-kafka',
-      version='0.2.4',
+      version='0.2.5',
       description='A Kafka plugin for Cabot by hxpham',
       author='hxpham',
       author_email='hxpham@etcc.com',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url = 'https://github.com/phamhungptithcm/cabot-alert-kafka/releases/tag/0.2.4.zip',
+      download_url = 'https://github.com/phamhungptithcm/cabot-alert-kafka/releases/tag/0.2.5.zip',
      )
