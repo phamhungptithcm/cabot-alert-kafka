f1405e41b837773e8c98f6e35e9998a122bd870c
Phạm Hùng <32831453+phamhungptithcm@users.noreply.github.com>
2020-12-28 04:00:43 -0600

    Update 0001_initial.py

--- cabot_alert_kafka/migrations/0001_initial.py
+++ cabot_alert_kafka/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='KafkaAlert',
             fields=[
-                ('alertplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
+                ('alertkafkaplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
             ],
             options={
                 'abstract': False,
