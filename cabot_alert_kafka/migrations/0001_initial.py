43fd33bf4a3e43554863fdfe536a9ebbe7db739b
Phạm Hùng <32831453+phamhungptithcm@users.noreply.github.com>
2020-12-28 20:37:05 -0600

    Update 0001_initial.py

--- cabot_alert_kafka/migrations/0001_initial.py
+++ cabot_alert_kafka/migrations/0001_initial.py
@@ -3,23 +3,20 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-class Migration(migrations.Migration):
+class Migration(migrations.Migration): 
     
-    initial = True
-
     dependencies = [
-        ('cabotapp', '0003_auto_20170201_1045'),
+        ('cabotapp', '0001_initial'),
     ]
-
     operations = [
         migrations.CreateModel(
             name='KafkaAlert',
-            fields=[
-                ('alertkafkaplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
+           fields=[
+                ('alertplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
             ],
             options={
                 'abstract': False,
             },
             bases=('cabotapp.alertplugin',),
-        )
+        ),
     ]
